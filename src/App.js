cd33cc7dbf447b74951ca30bc72ac72314e62b68
Adrian Marquis <adrian.marquis78@gmail.com>
2020-07-30 08:16:48 -0400

    Shopping List Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from "react";
-import Accordion from './state-drills/Accordion';
-
-
-// array of objects each with a name and content
-const sections = [
-  {
-    title: 'Section 1',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. And other stuff with stuff!',
-  },
-  {
-    title: 'Section 2',
-    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
-  },
-  {
-    title: 'Section 3',
-    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-  },
-]
-
+import AddItemForm from './shopping-list/AddItemForm';
+import ShoppingList from './shopping-list/ShoppingList';
 
 class App extends React.Component {
+  state = {
+    shoppingItems: [
+      /* put stub items in here for testing */
+      { name: 'apples', checked: false },
+      { name: 'oranges', checked: true },
+      { name: 'bread', checked: false },
+    ]
+  };
+
   render() {
-    return <div>
-      <Accordion sections={sections} />
-    </div>
+    return (
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          <section>
+            <AddItemForm />
+          </section>
+          <section>
+            <ShoppingList items={this.state.shoppingItems} />
+          </section>
+        </main>
+      </>
+    )
   }
 }
 
