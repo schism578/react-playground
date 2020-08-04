dd2f3368ea3872d682233487ea7b1fb4449183b1
Adrian Marquis <adrian.marquis78@gmail.com>
2020-08-04 09:09:16 -0400

    State included

--- src/demonymapp/demonymApp.js
+++ src/demonymapp/demonymApp.js
@@ -4,13 +4,43 @@ import './demonymApp.css'
 import Demonym from './demonym';
 import CountrySelector from './countrySelector';
 
-
 class DemonymApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+      selected: null
+    };
+  }
+
+  setSelected(selected) {
+    this.setState({
+      selected
+    });
+  }
+
+  componentDidMount() {
+    fetch('https://country.register.gov.uk/records.json?page-size=5000')
+      .then(response => response.json())
+      .then(data => {
+        const countries = Object.keys(data)
+              .map(key => data[key].item[0]);
+        this.setState({
+          countries
+        });
+      });
+  }
+
   render() {
+    const demon = this.state.selected
+          ? <Demonym name={this.state.selected['citizen-names']} country={this.state.selected.name}/>
+          : <div className="demonym_app__placeholder">Select a country above</div>;
+
     return (
       <div className="demonym_app">
-        <CountrySelector countries={[{name:"Barbados"}, {name:"Bahrain"}]}/>
-        <Demonym name="Barbadian" country="Barbados"/>
+        <CountrySelector countries={this.state.countries}/>
+        {demon}
       </div>
     );
   }
